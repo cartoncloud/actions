582cb0f1075b84aa1c7f49bcdf495b04158050a1
Aaron Wilson <aaron.wilson@cartoncloud.com.au>
2022-03-10 17:06:12 +1300

    Add update JIRA release action

---
+++ update-jira-release/README.md
@@ -0,0 +1,31 @@
+# Update JIRA Release
+Action to update an existing JIRA (self-hosted) release
+
+## Inputs
+
+### username (required)
+JIRA username to use to call the API
+
+### password (required)
+JIRA password to use to call the API
+
+### serverAddress (required)
+JIRA server address i.e. support.cartoncloud.com
+
+### projectId (required)
+The id of the JIRA project the release exists in
+
+### releaseId (required)
+JIRA release id to update
+
+### name (optional)
+Optional new name for the release
+
+### description (optional)
+Optional new description for the release
+
+### isReleased (required)
+Whether to mark the release as unreleased or released
+
+### timezone (optional)
+Timezone for the release date. Defaults to Australia/Brisbane
---
+++ update-jira-release/src/index.js
@@ -0,0 +1,52 @@
+const core = require('@actions/core');
+const fetch = require('node-fetch');
+const { DateTime } = require("luxon");
+
+async function run() {
+  try {
+    const username = core.getInput('username', { required: true });
+    const password = core.getInput('password', { required: true });
+    const serverAddress = core.getInput('serverAddress', { required: true });
+    const projectId = core.getInput('projectId', { required: true });
+    const releaseId = core.getInput('releaseId', { required: true });
+    const releaseName = core.getInput('name');
+    const releaseDescription = core.getInput('description');
+    const isReleased = core.getBooleanInput('isReleased', { required: true });
+    const timezone = core.getInput('timezone');
+
+    const today = DateTime.utc().setZone(timezone).toFormat('yyyy-MM-dd');
+    const body = {
+      projectId: projectId,
+      id: releaseId,
+      name: releaseName ? releaseName : undefined,
+      description: releaseDescription ? releaseDescription : undefined,
+      archived: false,
+      released: isReleased,
+      releaseDate: isReleased ? today : undefined,
+    };
+
+    core.info(`Updating release:\n${JSON.stringify(body, null, 2)}`);
+
+    const base64Credentials = btoa(`${username}:${password}`);
+    const response = await fetch(`https://${serverAddress}/rest/api/latest/version/${releaseId}`, {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Basic ${base64Credentials}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      core.info(`Updated release ${jsonResponse.id}`);
+    } else {
+      core.error(response.statusText);
+      core.setFailed(response.text());
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
