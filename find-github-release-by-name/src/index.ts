8d67a880df20cf8f51f31bc71fb80ed686191285
Aaron Wilson <aaron.wilson@cartoncloud.com.au>
2022-06-03 11:38:51 +1200

    Update release asset download

--- find-github-release-by-name/src/index.ts
+++ find-github-release-by-name/src/index.ts
@@ -1,6 +1,7 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
-import * as fs from "fs";
+import fetch from 'node-fetch';
+import { writeFile } from "fs/promises";
 
 async function run() {
   try {
@@ -33,17 +34,40 @@ async function run() {
         if (release.assets.length > 0 && releaseAsset) {
           const asset = release.assets.find((it) => it.name === releaseAsset);
           if (asset) {
-            const file = fs.createWriteStream(asset.name);
-            const buffer = await octokit.rest.repos.getReleaseAsset({
-              headers: {
-                Accept: 'application/octet-stream',
-              },
-              asset_id: asset.id,
-              repo: repo,
-              owner: owner,
-            });
-            file.write(buffer);
-            file.end();
+            const {
+              body,
+              headers: { accept, 'user-agent': userAgent },
+              method,
+              url,
+            } = octokit.request.endpoint(
+              'GET /repos/:owner/:repo/releases/assets/:asset_id',
+              {
+                asset_id: asset.id,
+                headers: {
+                  accept: 'application/octet-stream',
+                },
+                owner,
+                repo,
+              }
+            );
+            let headers: HeadersInit = {
+              accept,
+              authorization: `token ${token}`,
+            };
+
+            if (typeof userAgent !== 'undefined') {
+              headers = { ...headers, 'user-agent': userAgent };
+            }
+
+            const response = await fetch(url, { body, headers, method });
+            if (!response.ok) {
+              const text = await response.text();
+              core.warning(text);
+              throw new Error('Invalid response');
+            }
+            const blob = await response.blob();
+            const arrayBuffer = await blob.arrayBuffer();
+            void (await writeFile(asset.name, new Uint8Array(arrayBuffer)));
           } else {
             core.warning(`Unable to find asset with name ${releaseAsset}`)
           }
