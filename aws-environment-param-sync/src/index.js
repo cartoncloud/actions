f5f05dab05b76fc0f4a2d92cfcdf4a2ce63507d9
jake-naughton <jake.naughton@cartoncloud.com.au>
2023-09-11 12:06:35 +1000

    PLT-338 - Sync param store variables with GitHub environments - Adding action to test first version. - Incomplete, still needs further testing.

---
+++ aws-environment-param-sync/src/index.js
@@ -0,0 +1,62 @@
+const github = require('@actions/github');
+const core = require('@actions/core');
+const { SSM } = require('aws-sdk');
+
+const getParameter = async ({
+     environmentPath,
+  }) => {
+  const ssm = new SSM();
+  const parameters = {};
+  let nextToken = undefined;
+
+  do {
+    const { Parameters, NextToken } = await ssm
+      .getParametersByPath({
+        Path: environmentPath,
+        Recursive: true,
+        WithDecryption: false,
+        NextToken: nextToken,
+      })
+      .promise();
+
+    Parameters.forEach((parameter) => {
+      const name = formatParameterName(parameter.Name);
+      const value = parameter.Value.trim();
+
+      parameters[name] = value;
+    });
+
+    nextToken = NextToken;
+  } while (Boolean(nextToken));
+
+  return parameters;
+};
+
+const formatParameterName = (name) => {
+  const splitName = name.split('/');
+  let formattedName = splitName[splitName.length-1];
+  formattedName = formattedName.toUpperCase();
+
+  return formattedName;
+}
+
+async function run() {
+  try {
+    core.info('Starting...');
+    const environmentPath = core.getInput('environmentPath', { required: true });
+    const environmentVariablesPath = core.getInput('environmentVariablesPath', { required: true });
+
+    core.info('EnvironmentPath: ' + environmentPath);
+    core.info('environmentVariablesPath: ' + environmentVariablesPath);
+    const parameters = await Promise.all(paths.map(path => getParameter({ environmentPath })));
+
+    parameters.forEach((parameter) => {
+      core.info(parameter);
+    });
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
